9ccff660174224600ba49717e3baffdcad466c76
shalkovy <shalkovy@gmail.com>
2019-06-24 13:35:29 +0300

    Initial Commit

---
+++ GoogleSearchApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoogleSearchApp
+//
+//  Created by Dima Shelkov on 6/24/19.
+//  Copyright © 2019 Dima Shelkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
